953130ca7c2a8293384992790f53828fd2a03fd9
Lucas Aguiar <7lucasdaniel@gmail.com>
2024-03-19 17:52:41 -0300

    Add project files.

---
+++ Models/Users.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiDiflenStore.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; }
+        public int UserLevel { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
