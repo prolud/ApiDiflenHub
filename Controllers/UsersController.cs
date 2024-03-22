a6c6b7ba7c32ef11b1809364078e371bf0b1079b
Lucas Aguiar <7lucasdaniel@gmail.com>
2024-03-22 20:33:39 -0300

    feat: mudança de regra de negocio

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -16,51 +16,45 @@ namespace ApiDiflenStore.Controllers
         }
 
         [HttpPost("add-user")]
-        public async Task<IActionResult> CreateUser(string username, string password, int userLevel)
+        public async Task<IActionResult> CreateUser(string email, string password, int userLevel)
         {
             try
             {
                 var user = new Users()
                 {
-                    Username = username,
+                    Email = email,
                     Password = password,
                     UserLevel = userLevel,
                     CreationDate = DateTime.Now
                 };
 
-                _appDbContext.Users.Add(user);
-                await _appDbContext.SaveChangesAsync();
+                if (_appDbContext.Users.Where(_ => _.Email.ToLower() == user.Email).Any())
+                {
+                    return BadRequest("Email already exists.");
+                }
+                else
+                {
+                    _appDbContext.Users.Add(user);
+                    await _appDbContext.SaveChangesAsync();
+                    return Ok(user);
+                }
 
-                return Ok(user);
             }
             catch (Exception ex)
             {
                 return BadRequest(ex);
             }
 
-        }
-
-        [HttpGet("get-users")]
-        public async Task<IActionResult> GetUsers()
-        {
-            try
-            {
-                var users = await _appDbContext.Users.ToListAsync();
-
-                return Ok(users);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex);
-            }
         }
 
         [HttpDelete("delete-user")]
-        public async Task<IActionResult> DeleteUser(string username, string password)
+        public async Task<IActionResult> DeleteUser(string email, string password)
         {
             try
             {
-                await _appDbContext.Users.Where(_ => _.Username == username && _.Password == password).ExecuteDeleteAsync();
+                await _appDbContext.Users
+                    .Where(_ => _.Email == email && _.Password == password)
+                    .ExecuteDeleteAsync();
 
                 return Ok();
             }
@@ -70,16 +64,20 @@ namespace ApiDiflenStore.Controllers
             }
         }
 
-        [HttpPut("update-user")]
-        public async Task<IActionResult> UpdateUser(string oldUsername, string oldPassword, string newUsername, string newPassword)
+        [HttpPut("update-password")]
+        public async Task<IActionResult> UpdatePassword(string email, string oldPassword, string newPassword)
         {
             try
             {
                 var user = _appDbContext.Users
-                    .Where(_ => _.Username == oldUsername && _.Password == oldPassword)
-                    .First();
+                    .Where(_ => _.Email == email && _.Password == oldPassword)
+                    .FirstOrDefault();
 
-                user.Username = newUsername;
+                if(user == null)
+                {
+                    return Unauthorized("Invalid email or password.");
+                }
+
                 user.Password = newPassword;
                 await _appDbContext.SaveChangesAsync();
 
--- Models/Users.cs
+++ Models/Users.cs
@@ -8,7 +8,7 @@ namespace ApiDiflenStore.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public int UserLevel { get; set; }
         public DateTime CreationDate { get; set; }
