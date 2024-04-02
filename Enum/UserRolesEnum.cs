260d54c3e6458a3df7ec02f1cf47e77267b0a06e
Lucas Aguiar <7lucasdaniel@gmail.com>
2024-04-02 11:35:25 -0300

    criando enum

---
+++ Enum/UserRolesEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiDiflenStore.Enum
+{
+    public class UserRolesEnum
+    {
+        public enum IdEnum
+        {
+            Commom = 1,
+            Intermediary = 2,
+            Advanced = 3
+        }
+    }
+}
