a812d2d8b1a0da90885fd5aeaa1f3ab2adc8e433
Lucas Aguiar <7lucasdaniel@gmail.com>
2024-10-04 21:36:51 -0300

    att .net

---
+++ ApiDiflenHub/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiDiflenStore.Controllers
+{
+    public class LogginRequestBody
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("say-hello")]
+        public IActionResult SayHello()
+        {
+            return Ok("Hello, World!");
+        }
+    }
+}
