7c035fd7e0c29b421abf343ff3dd0f88913c12a0
Lucas Aguiar <7lucasdaniel@gmail.com>
2025-06-30 10:58:07 -0300

    feat: adicionando middleware

---
+++ API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace API;
+
+public class ExceptionMiddleware(RequestDelegate _next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new
+            {
+                context.Response.StatusCode,
+                Message = $"Erro interno do servidor: {ex.Message}",
+            };
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            await context.Response.WriteAsync(JsonSerializer.Serialize(response, options));
+        }
+    }
+}
